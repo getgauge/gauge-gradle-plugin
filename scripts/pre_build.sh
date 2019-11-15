1a06bef14f3fe384a37cb3a2bea6a4b733f67033
Subham Singh <s.chauhan5505@gmail.com>
2019-11-15 14:35:54 +0530

    Remove bintray task from build.gradle

--- scripts/pre_build.sh
+++ scripts/pre_build.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
-echo deb https://dl.bintray.com/gauge/gauge-deb stable main | sudo tee -a /etc/apt/sources.list
+
 sudo apt-get update
 sudo apt-get install gauge
-gauge_setup
-gauge --install java
-echo "bintray.user=user\nbintray.apikey=key" > local.properties
+gauge install java
