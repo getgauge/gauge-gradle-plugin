08f84920a77923611d1496aa262f40650cbadc6f
Nivedha <nivedhasenthil@gmail.com>
2020-04-21 17:32:44 +0530

    Get classpath from sourceSet (#47)
    * update gradle to 6.3
    
    * Get classpath from sourceSet #41
    
    * Remove compile time dependencies
    
    Signed-off-by: NivedhaSenthil <nivedhasenthil@gmail.com>

--- plugin/src/test/java/com/thoughtworks/gauge/gradle/util/PropertyManagerTest.java
+++ plugin/src/test/java/com/thoughtworks/gauge/gradle/util/PropertyManagerTest.java
@@ -2,8 +2,8 @@ package com.thoughtworks.gauge.gradle.util;
 
 import com.thoughtworks.gauge.gradle.GaugeExtension;
 import org.gradle.api.Project;
-import org.gradle.api.artifacts.Configuration;
-import org.gradle.api.artifacts.ConfigurationContainer;
+import org.gradle.api.tasks.SourceSet;
+import org.gradle.api.tasks.SourceSetContainer;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -11,9 +11,8 @@ import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import java.io.File;
-import java.util.Arrays;
-import java.util.LinkedHashSet;
-import java.util.stream.Collectors;
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.junit.Assert.assertThat;
@@ -26,48 +25,48 @@ public class PropertyManagerTest {
 
     @Mock
     private Project project;
+
     @Mock
-    private ConfigurationContainer configContainer;
+    private SourceSetContainer sourceSetContainer;
 
     private GaugeExtension extension;
 
     @Before
     public void setUp() {
         extension = new GaugeExtension();
-
-        when(project.getConfigurations()).thenReturn(configContainer);
-        when(project.getBuildDir()).thenReturn(new File("/blah"));
+        Map<String,Object> properties = new HashMap<String, Object>();
+        properties.put("sourceSets",sourceSetContainer);
+        doReturn(properties).when(project).getProperties();
     }
 
     @Test
     public void classpathShouldBeEmptyIfNoTesRuntimeDependencies() {
-        mockTestRuntimeClasspathConfiguration();
-        PropertyManager manager = new PropertyManager(project, extension);
+        SourceSet config = mock(SourceSet.class, RETURNS_DEEP_STUBS);
+        when(config.getName()).thenReturn("");
+        when(config.getRuntimeClasspath().getAsPath())
+                .thenReturn("");
+        when(sourceSetContainer.getByName("test")).thenReturn(config);
+         PropertyManager manager = new PropertyManager(project, extension);
 
-        manager.setProperties();
+         manager.setProperties();
 
-        assertThat(extension.getClasspath(), containsString(""));
+         assertThat(extension.getClasspath(), containsString(""));
     }
 
     @Test
     public void classpathShouldIncludeTestRuntimeClasspathConfigurations() {
-        mockTestRuntimeClasspathConfiguration("blah.jar", "blah2.jar");
-        PropertyManager manager = new PropertyManager(project, extension);
-
-        manager.setProperties();
-
-        assertThat(extension.getClasspath(), containsString(String.join(File.pathSeparator, "blah.jar", "blah2.jar")));
-    }
-
-    private void mockTestRuntimeClasspathConfiguration(String... files) {
-        Configuration config = mock(Configuration.class, RETURNS_DEEP_STUBS);
+        SourceSet config = mock(SourceSet.class, RETURNS_DEEP_STUBS);
         when(config.getName()).thenReturn("");
-        when(config.getAsFileTree().getFiles())
-                .thenReturn(new LinkedHashSet<>(
-                        Arrays.stream(files)
-                                .map(File::new)
-                                .collect(Collectors.toList())));
-        when(configContainer.getByName("testRuntimeClasspath")).thenReturn(config);
+        when(config.getRuntimeClasspath().getAsPath())
+                .thenReturn(String.join(File.pathSeparator, "blah.jar", "blah2.jar"));
+        when(sourceSetContainer.getByName("test")).thenReturn(config);
+         PropertyManager manager = new PropertyManager(project, extension);
+
+         manager.setProperties();
+
+         assertThat(extension.getClasspath(), containsString(String.join(File.pathSeparator, "blah.jar", "blah2.jar")));
     }
+
+
 
 }
