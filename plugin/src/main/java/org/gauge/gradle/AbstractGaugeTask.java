a503535cc763f2077079f6d7a87aff62668cc206
Haroon Sheikh <haroon.sheikh@outlook.com>
2025-11-28 13:46:07 +0000

    Update Gradle wrapper to 9.2.1 and resolve pmd issues
    Signed-off-by: Haroon Sheikh <haroon.sheikh@sky.uk>

--- plugin/src/main/java/org/gauge/gradle/AbstractGaugeTask.java
+++ plugin/src/main/java/org/gauge/gradle/AbstractGaugeTask.java
@@ -11,22 +11,44 @@ import org.gradle.work.DisableCachingByDefault;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * Abstract base class for Gauge tasks, providing common functionality for Gauge-related Gradle tasks.
+ */
 @DisableCachingByDefault
 public abstract class AbstractGaugeTask extends DefaultTask {
-
+    /**
+     * Logger instance for Gauge tasks.
+     */
     protected static final Logger logger = LoggerFactory.getLogger("gauge");
+    /**
+     * ExecOperations for executing external processes.
+     */
     protected final ExecOperations execOps;
+    /**
+     * The Gradle project associated with this task.
+     */
     protected final Project project;
-
+    /**
+     * Constructs an AbstractGaugeTask.
+     *
+     * @param execOps     the ExecOperations instance
+     * @param project     the Gradle project
+     * @param description the task description
+     */
     @Inject
-    public AbstractGaugeTask(final ExecOperations execOps, final Project project, final String description) {
+    protected AbstractGaugeTask(final ExecOperations execOps, final Project project, final String description) {
         this.execOps = execOps;
         this.project = project;
         super.setGroup(GaugeConstants.GAUGE_TASK_GROUP);
         super.setDescription(description);
         super.dependsOn("classes", "testClasses");
     }
-
+    /**
+     * Configures the ExecSpec for the Gauge command.
+     *
+     * @param spec    the ExecSpec to configure
+     * @param command the GaugeCommand instance
+     */
     protected abstract void configureSpec(final ExecSpec spec, final GaugeCommand command);
 
     private String getClasspath() {
@@ -36,6 +58,9 @@ public abstract class AbstractGaugeTask extends DefaultTask {
             .getAsPath();
     }
 
+    /**
+     * Executes the Gauge task.
+     */
     @TaskAction
     public void execute() {
         final GaugeExtension extension = project.getExtensions().findByType(GaugeExtension.class);
--- plugin/src/main/java/org/gauge/gradle/GaugeClasspathTask.java
+++ plugin/src/main/java/org/gauge/gradle/GaugeClasspathTask.java
@@ -11,22 +11,36 @@ import org.gradle.api.DefaultTask;
 import org.gradle.api.Project;
 import org.gradle.api.tasks.SourceSetContainer;
 import org.gradle.api.tasks.TaskAction;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+/**
+ * Task to print the test runtime classpath for Gauge.
+ */
 public abstract class GaugeClasspathTask extends DefaultTask {
 
+    private static final Logger logger = LoggerFactory.getLogger("gauge");
     private final Project project;
 
+    /**
+     * Constructs a GaugeClasspathTask.
+     *
+     * @param project the Gradle project
+     */
     @Inject
-    public GaugeClasspathTask(final Project project) {
+    protected GaugeClasspathTask(final Project project) {
         this.project = project;
         super.setGroup(GaugeConstants.GAUGE_TASK_GROUP);
         super.setDescription("Gets the classpath.");
         super.dependsOn("build");
     }
 
+    /**
+     * Prints the test runtime classpath to the logger.
+     */
     @TaskAction
     public void classpath() {
-        System.out.println(project.getExtensions().getByType(SourceSetContainer.class)
+        logger.info(project.getExtensions().getByType(SourceSetContainer.class)
             .getByName("test")
             .getRuntimeClasspath()
             .getAsPath());
