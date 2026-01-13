3b8bfff6c47a3f477de35191689aa709509e8993
Chad Wilson <29788154+chadlwilson@users.noreply.github.com>
2026-01-13 17:39:39 +0800

    Remove v7

--- plugin/src/integrationTest/java/org/gauge/gradle/TestGradleVersions.java
+++ plugin/src/integrationTest/java/org/gauge/gradle/TestGradleVersions.java
@@ -3,7 +3,6 @@ package org.gauge.gradle;
 import org.gradle.util.GradleVersion;
 
 public enum TestGradleVersions {
-    v7_6("7.6.6"), // Jul 2025
     v8_14("8.14.1"), // May 2025
     vCURRENT(GradleVersion.current().getVersion());
 
