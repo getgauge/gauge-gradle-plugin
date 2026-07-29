525f868e19e9fe502c4ad08563444474d5c50503
Haroon Sheikh <haroon.sheikh@sky.uk>
2026-07-29 18:27:49 +0100

    Refactor Gauge tasks to improve caching behavior and update Gradle wrapper to 9.6.1
    Signed-off-by: Haroon Sheikh <haroon.sheikh@sky.uk>

--- plugin/src/main/java/org/gauge/gradle/GaugeClasspathTask.java
+++ plugin/src/main/java/org/gauge/gradle/GaugeClasspathTask.java
@@ -9,14 +9,14 @@ package org.gauge.gradle;
 import javax.inject.Inject;
 import org.gradle.api.DefaultTask;
 import org.gradle.api.Project;
-import org.gradle.api.tasks.CacheableTask;
 import org.gradle.api.tasks.SourceSetContainer;
 import org.gradle.api.tasks.TaskAction;
+import org.gradle.work.DisableCachingByDefault;
 
 /**
  * Task to print the test runtime classpath for Gauge.
  */
-@CacheableTask
+@DisableCachingByDefault(because = "Does not produce cacheable outputs.")
 public abstract class GaugeClasspathTask extends DefaultTask {
 
     private final Project project;
